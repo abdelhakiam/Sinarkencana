aaa81eb066c3cda3fb7370a63776bf9523a110de
itsgitz <anggit.ginanjar@outlook.com>
2023-01-30 18:51:33 +0700

    chore: upgrade all modules to 15

--- sales_commission_generic/__manifest__.py
+++ sales_commission_generic/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name" : "Sales Commission from Sales/Invoice/Payment in Odoo ",
-    "version" : "14.0.0.1",
+    "version" : "15.0.0.0",
     'category' : "Sales",
     "summary" : "Sale Commission for sales order invoice based commission payment based commission margin based commission for product margin commissions for sales person commission for partner Sales Agent commission Sales Commission for Users commission based on margin",
     "description": """

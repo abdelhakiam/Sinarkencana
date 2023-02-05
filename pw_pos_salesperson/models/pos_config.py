0d612208707a773dd404ca79382fd3b666bee0c4
itsgitz <anggit.ginanjar@outlook.com>
2023-02-05 14:23:56 +0700

    Revert "feat: try pw_pos_salesperson"
    This reverts commit f3e685d734738f4948c546ee074190cc25d6d987.

--- pw_pos_salesperson/models/pos_config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    allow_salesperson = fields.Boolean('Allow Salesperson')

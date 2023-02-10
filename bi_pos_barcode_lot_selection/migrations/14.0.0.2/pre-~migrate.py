7a04d234f03bf35ff9595a8ecae697b0d948bf23
itsgitz <anggit.ginanjar@outlook.com>
2023-02-10 18:17:37 +0700

    chore: added post-migrate test

--- bi_pos_barcode_lot_selection/migrations/14.0.0.2/pre-~migrate.py
+++ bi_pos_barcode_lot_selection/migrations/14.0.0.2/pre-~migrate.py
@@ -2,4 +2,4 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    print('hello from pre-migration of bi_pos_barcode_lot_selection!')
+    print('hello from pre-migration/post-upgrade of bi_pos_barcode_lot_selection!')
